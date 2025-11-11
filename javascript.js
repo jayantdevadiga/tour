2fadce53a1ab7c8bb7690d012d0541b63799e236
jayantdevadiga <jayantdevadiga44@gmail.com>
2025-11-11 23:24:58 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,31 @@
+var  num2 = 9;
+var num2="hello"
+console.log(num2)
+// scopes in jvascript
+let char1= "5"
+let num1 = 5
+
+console.log(char1+num1)
+
+if (char1==num1){
+    console.log("yes")
+}
+else{
+    console.log("no")
+}
+
+let n = 13
+
+if (n%3==0 && n%5==0){
+    console.log("Jayant and kumar and Nithiesh")
+}
+else if(n%5==0){
+    console.log("Kumar and Nithiesh")
+}
+else{
+    console.log("Number is not valid")
+}
+
+
+let arr = ["proName","proPrice","proColor"];
+console.log(arr[2])
